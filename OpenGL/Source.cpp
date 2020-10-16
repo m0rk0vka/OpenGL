6d801c8884d187600d2144ecf3d934ec8211cd92
m0rk0vka <vovatatosyan2000@mail.ru>
2020-10-16 15:47:04 +0300

    Add project files.

---
+++ OpenGL/Source.cpp
@@ -0,0 +1,82 @@
+// GLEW 
+#define GLEW_STATIC 
+#include <GL/glew.h> 
+//#include <glad/glad.h> 
+// GLFW 
+#include <glfw3.h> 
+
+// GL includes 
+#include <shader.hpp> 
+#include <camera.hpp> 
+
+// GLM Mathemtics 
+#include <glm/glm.hpp> 
+#include <glm/gtc/matrix_transform.hpp> 
+#include <glm/gtc/type_ptr.hpp> 
+#include <map> 
+#include <vector> 
+#include <algorithm> 
+// Other Libs 
+#include <src/SOIL.h> 
+#include <stb_image.h>
+
+#include <iostream>
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+int main()
+{
+	//Инициализация GLFW
+	glfwInit();
+	//Настройка GLFW
+	//Задается минимальная требуемая версия OpenGL. 
+	//Мажорная 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	//Минорная
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	//Установка профайла для которого создается контекст
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//Выключение возможности изменения размера окна
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	//Создаем объект окна
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "Failed to initialize GLEW" << std::endl;
+		return -1;
+	}
+
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+
+	glViewport(0, 0, width, height);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	// Когда пользователь нажимает ESC, мы устанавливаем свойство WindowShouldClose в true, 
+	// и приложение после этого закроется
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+}
